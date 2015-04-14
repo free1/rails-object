96f3920efe557b76e83e87a9a16e140955114137
free1 <747549945@qq.com>
2015-04-14 16:36:27 +0800

    暂时去除不需要的链接

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,42 +45,42 @@ set :scm, :git
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
 # for details of operations
-set(:config_files, %w(
-  nginx.conf
-  database.example.yml
-  log_rotation
-  monit
-  unicorn.rb
-  unicorn_init.sh
-))
+# set(:config_files, %w(
+#   nginx.conf
+#   database.example.yml
+#   log_rotation
+#   monit
+#   unicorn.rb
+#   unicorn_init.sh
+# ))
 
 # which config files should be made executable after copying
 # by deploy:setup_config
-set(:executable_config_files, %w(
-  unicorn_init.sh
-))
+# set(:executable_config_files, %w(
+#   unicorn_init.sh
+# ))
 
 # files which need to be symlinked to other parts of the
 # filesystem. For example nginx virtualhosts, log rotation
 # init scripts etc.
-set(:symlinks, [
-  {
-    source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{fetch(:application)}"
-  },
-  {
-    source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{fetch(:application)}"
-  },
-  {
-    source: "log_rotation",
-   link: "/etc/logrotate.d/#{fetch(:application)}"
-  },
-  {
-    source: "monit",
-    link: "/etc/monit/conf.d/#{fetch(:application)}.conf"
-  }
-])
+# set(:symlinks, [
+#   {
+#     source: "nginx.conf",
+#     link: "/etc/nginx/sites-enabled/#{fetch(:application)}"
+#   },
+#   {
+#     source: "unicorn_init.sh",
+#     link: "/etc/init.d/unicorn_#{fetch(:application)}"
+#   },
+#   {
+#     source: "log_rotation",
+#    link: "/etc/logrotate.d/#{fetch(:application)}"
+#   },
+#   {
+#     source: "monit",
+#     link: "/etc/monit/conf.d/#{fetch(:application)}.conf"
+#   }
+# ])
 
 
 # Default value for :format is :pretty
