9115a03fc202156ef49c71766ad871c25125b4c4
free1 <747549945@qq.com>
2015-04-15 19:09:32 +0800

    capistrano test

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,6 @@ set :scm, :git
 set(:config_files, %w(
   nginx.conf
   database.example.yml
-  monit
   unicorn.rb
   unicorn_init.sh
 ))
@@ -131,20 +130,20 @@ set :keep_releases, 5
 
 namespace :deploy do
   # make sure we're deploying what we think we're deploying
-  before :deploy, "deploy:check_revision"
+  # before :deploy, "deploy:check_revision"
   # only allow a deploy with passing tests to deployed
-  before :deploy, "deploy:run_tests"
+  # before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
   # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
   # remove the default nginx configuration as it will tend
   # to conflict with our configs.
-  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+  # before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
   # reload nginx to it will pick up any modified vhosts from
   # setup_config
-  after 'deploy:setup_config', 'nginx:reload'
+  # after 'deploy:setup_config', 'nginx:reload'
 
   # Restart monit so it will pick up any monit configurations
   # we've added
