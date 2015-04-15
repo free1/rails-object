258f7bf24eae9e4fa9f00f66f7aaebc21b523b41
free1 <747549945@qq.com>
2015-04-15 18:11:49 +0800

    test capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,7 +93,7 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
@@ -122,28 +122,28 @@ set :keep_releases, 5
 # end
 
 
-# namespace :deploy do
-#   # make sure we're deploying what we think we're deploying
-#   before :deploy, "deploy:check_revision"
-#   # only allow a deploy with passing tests to deployed
-#   before :deploy, "deploy:run_tests"
-#   # compile assets locally then rsync
-#   # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
-#   after :finishing, 'deploy:cleanup'
+namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, "deploy:check_revision"
+  # only allow a deploy with passing tests to deployed
+  before :deploy, "deploy:run_tests"
+  # compile assets locally then rsync
+  # after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
-#   # remove the default nginx configuration as it will tend
-#   # to conflict with our configs.
-#   before 'deploy:setup_config', 'nginx:remove_default_vhost'
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
-#   # reload nginx to it will pick up any modified vhosts from
-#   # setup_config
-#   after 'deploy:setup_config', 'nginx:reload'
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
 
-#   # Restart monit so it will pick up any monit configurations
-#   # we've added
-#   after 'deploy:setup_config', 'monit:restart'
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  after 'deploy:setup_config', 'monit:restart'
 
-#   # As of Capistrano 3.1, the `deploy:restart` task is not called
-#   # automatically.
-#   after 'deploy:publishing', 'deploy:restart'
-# end
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
+end
