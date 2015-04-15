561effdec88a70e7001d407118ac88fa43539424
free1 <747549945@qq.com>
2015-04-15 16:46:48 +0800

    修改unicorn应用地址

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,7 +5,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/vagrant
+APP_ROOT=/home/deploy/apps/weixin_test/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
