b926c0bd8f4504c58330a80d1769245d464c9301
free1 <747549945@qq.com>
2015-04-22 19:03:07 +0800

    抓取使用rails runner

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
+require 'free_spider'
+
 class Post < ActiveRecord::Base
 
 	belongs_to :user
 	validates :title, presence: true, uniqueness: true
 	validates :content, presence: true
+
+	def self.crawl_post
+		spider = FreeSpider::Begin.new
+		spider.plan do
+		  site 'http://oszine.com/'
+		end
+		spider.crawl
+	end
+	
 end
