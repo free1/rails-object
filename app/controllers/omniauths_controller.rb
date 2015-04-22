eae982aca2a71e9d25aa92ceec85b1b9322b66ce
free1 <747549945@qq.com>
2015-04-22 10:28:15 +0800

    加上微博登录验证

--- app/controllers/omniauths_controller.rb
+++ app/controllers/omniauths_controller.rb
@@ -3,7 +3,7 @@ class OmniauthsController < ApplicationController
 	def create
 		user = User.from_auth(request.env['omniauth.auth'])
 		sign_in user
-		flash.now[:notice] = "Welcome #{user.name}"
+		flash.now[:success] = "Welcome #{user.name}"
 		redirect_to root_path
 	end
 end
