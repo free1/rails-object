e54b49b8d6fdd3d2b88ebb5aadd2018c6d27e08b
free1 <747549945@qq.com>
2015-04-24 11:09:37 +0800

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :use_sudo, false
 set :user, "deploy"
 
 # rbenv
-set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_type, :system # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.2'
 # # in case you want to set ruby version from the file:
 # # set :rbenv_ruby, File.read('.ruby-version').strip
