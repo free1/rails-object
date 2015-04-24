9427115299b6f1bdc4923c428ea6fc50dce2fe95
free1 <747549945@qq.com>
2015-04-24 11:15:33 +0800

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,6 @@ set :rbenv_ruby, '2.1.2p95'
 # # set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-p "-----------"
-p fetch(:rbenv_path)
-
 set :rbenv_roles, :all # default value
 
 # asset migrate
