f9db579746c4911a2dad173f33d85a8bcabace25
free1 <747549945@qq.com>
2015-04-24 11:53:52 +0800

    post删除不必要内容

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,23 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true, uniqueness: true
 	validates :content, presence: true
 
-	def self.crawl_post
-		spider = FreeSpider::Begin.new
-		spider.plan do
-		  site 'http://oszine.com/'
+	class << self
+		# 抓取内容
+		def crawl_post
+			spider = FreeSpider::Begin.new
+			spider.plan do
+			  site 'http://oszine.com/'
+			end
+			spider.crawl
 		end
-		spider.crawl
+		# 删除不必要内容
+		def check_content
+			Post.find_each do |post|
+				content = post.content.split("<div class=\"loc_link\">\n<ul>\r\n")[0]
+				post.update_column(:content, content)
+			end
+		end
+
 	end
 	
 end
