fba6148ba709e4196b3d18a57431cea7a93eec79
free1 <747549945@qq.com>
2015-04-29 15:56:05 +0800

    富文本编辑器图片上传后替换地址

--- app/controllers/upload/qiniu_controller.rb
+++ app/controllers/upload/qiniu_controller.rb
@@ -2,7 +2,7 @@ class Upload::QiniuController < ApplicationController
 
 	# /upload/qiniu/image_up_token
 	def image_up_token
-		rb = %Q({"image_path":$(key),"hash":$(hash),"width":$(imageInfo.width),"height":$(imageInfo.height)})
+		rb = %Q({"image_path":$(key),"hash":$(hash),"width":$(imageInfo.width),"height":$(imageInfo.height), "file_path":"#{QiniuUploadUrl}/$(key)"})
 		uptoken = Qiniu.generate_upload_token(:scope => QiniuScope, return_body: rb, deadline: (Time.now + 30.seconds).to_i, :expires_in => QiniuExpiresIn)
 
 		render json: {uptoken: uptoken}
