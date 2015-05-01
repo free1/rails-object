40a2c8925230d0fcd0ef0eee6ef9ba88c8ab1abc
free1 <747549945@qq.com>
2015-05-01 09:43:13 +0800

    修复cap中定时任务

--- config/deploy.rb
+++ config/deploy.rb
@@ -121,7 +121,9 @@ set :bundle_binstubs, nil
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+# whenever
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+set :whenever_roles,        ->{ :all }
 
 before 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
