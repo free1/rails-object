aadaf76e31d985d716110e23a853719ee04bf88d
free1 <747549945@qq.com>
2015-05-06 13:35:06 +0800

    捕获api异常返回

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,3 +1,4 @@
+# encoding:utf-8
 require 'grape-swagger'
 
 class API < Grape::API
@@ -18,7 +19,52 @@ class API < Grape::API
           # Logger.new("#{Rails.root}/log/development.log")
         end
       end
+
+      # 手动捕获的异常, 在这里返回
+      def format_err_msg(message, status)
+        {
+            meta: {
+                err_msg: message,
+                result: false,
+                status_code: status
+            }
+        }.to_json
+      end
     end
+
+
+    rescue_from ActiveRecord::RecordNotFound do
+      rack_response(API.format_err_msg('数据不存在.', 404), 404)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |error|
+      rack_response(API.format_err_msg("数据验证错误: #{error}", 409), 409)
+    end
+
+    rescue_from Grape::Exceptions::ValidationErrors do |e|
+      rack_response(API.format_err_msg("参数错误: #{e.message}", e.status), e.status)
+    end
+
+    rescue_from JSON::ParserError do |e|
+      rack_response(API.format_err_msg("JSON解析出错: #{e.message}", e.status), e.status)
+    end
+
+    # rescue_from Grape::Exceptions::AboriginalError do |e|
+    #   Rack::Response.new({
+    #     error_code: e.status,
+    #   }.merge(e.message).to_json, e.status).finish
+    # end
+
+    rescue_from :all do |exception|
+      trace = exception.backtrace
+
+      message = "\n#{exception.class} (#{exception.message}):\n"
+      message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
+      message << "  " << trace.join("\n  ")
+      API.logger.add Logger::FATAL, message
+      rack_response(API.format_err_msg("未知错误", 500), 500)
+    end
+
 
     mount ::V1::Base
 
