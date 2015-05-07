41112c2705b971aaa75c202ff50b627b1cfac4c3
free1 <747549945@qq.com>
2015-05-07 19:30:43 +0800

    api加上栏目登录验证

--- app/api/v1/wei_xin/articles.rb
+++ app/api/v1/wei_xin/articles.rb
@@ -32,12 +32,14 @@ module V1
 
           desc '日知栏目关注'
           post '/collect' do
+            authenticate!
             current_user.like!(params[:id], 'Article')
             present :result, true
           end
 
           desc '日知栏目取消关注'
           delete '/cancel_collect' do
+            authenticate!
             current_user.cancel_like!(params[:id], 'Article')
             present :result, true
           end
@@ -47,6 +49,7 @@ module V1
             requires :content, type: String
           end
           post '/comment' do
+            authenticate!
             article = Article.find(params[:id])
             comment = article.comments.build(content: params[:content], user_id: current_user.id)
             if comment.save
