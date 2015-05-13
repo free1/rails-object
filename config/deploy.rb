0c3f7ce95f56b57a680718b96990c5eb90688ef7
free1 <747549945@qq.com>
2015-05-13 15:38:24 +0800

    暂时取消capistrano重启god

--- config/deploy.rb
+++ config/deploy.rb
@@ -139,63 +139,63 @@ namespace :deploy do
   end
 end
 
-# god
-namespace :god do
-  def god_is_running
-    capture(:bundle, "exec god status > /dev/null 2>&1 || echo 'god not running'") != 'god not running'
-  end
+# # god
+# namespace :god do
+#   def god_is_running
+#     capture(:bundle, "exec god status > /dev/null 2>&1 || echo 'god not running'") != 'god not running'
+#   end
  
-  # Must be executed within SSHKit context
-  # 暂时只监控unicorn，nginx由monit监控
-  def config_file
-    "#{release_path}/config/god/unicorn.god"
-  end
+#   # Must be executed within SSHKit context
+#   # 暂时只监控unicorn，nginx由monit监控
+#   def config_file
+#     "#{release_path}/config/god/unicorn.god"
+#   end
  
-  # Must be executed within SSHKit context
-  def start_god
-    execute :bundle, "exec god -c #{config_file}"
-  end
+#   # Must be executed within SSHKit context
+#   def start_god
+#     execute :bundle, "exec god -c #{config_file}"
+#   end
  
-  desc "Start god and his processes"
-  task :start do
-    on roles(:web) do
-      within release_path do
-        with RAILS_ENV: fetch(:rails_env) do
-          start_god
-        end
-      end
-    end
-  end
+#   desc "Start god and his processes"
+#   task :start do
+#     on roles(:web) do
+#       within release_path do
+#         with RAILS_ENV: fetch(:rails_env) do
+#           start_god
+#         end
+#       end
+#     end
+#   end
  
-  desc "Terminate god and his processes"
-  task :stop do
-    on roles(:web) do
-      within release_path do
-        if god_is_running
-          execute :bundle, "exec god terminate"
-        end
-      end
-    end
-  end
+#   desc "Terminate god and his processes"
+#   task :stop do
+#     on roles(:web) do
+#       within release_path do
+#         if god_is_running
+#           execute :bundle, "exec god terminate"
+#         end
+#       end
+#     end
+#   end
  
-  desc "Restart god's child processes"
-  task :restart do
-    on roles(:web) do
-      within release_path do
-        with RAILS_ENV: fetch(:rails_env) do
-          if god_is_running
-            execute :bundle, "exec god load #{config_file}"
-            execute :bundle, "exec god restart"
-          else
-            start_god
-          end
-        end
-      end
-    end
-  end
-end
+#   desc "Restart god's child processes"
+#   task :restart do
+#     on roles(:web) do
+#       within release_path do
+#         with RAILS_ENV: fetch(:rails_env) do
+#           if god_is_running
+#             execute :bundle, "exec god load #{config_file}"
+#             execute :bundle, "exec god restart"
+#           else
+#             start_god
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
 
-after "deploy:updated", "god:restart"
+# after "deploy:updated", "god:restart"
 
 # after 'deploy:publishing', 'unicorn:stop'
 # after 'unicorn:stop', 'unicorn:start'
