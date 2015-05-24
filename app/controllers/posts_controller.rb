39513c2e11d0b21f0d4f398b0f06bcb4067335b8
free1 <747549945@qq.com>
2015-05-24 15:47:11 +0800

    一个站抓取多了评论，暂时正序排列

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
 	def index
-		@posts = Post.order(id: :desc).paginate(page: params[:page], per_page: 12)
+		# @posts = Post.order(id: :desc).paginate(page: params[:page], per_page: 12)
+		@posts = Post.paginate(page: params[:page], per_page: 12)
 	end
 
 	def show
