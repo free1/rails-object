4f21b99ee5b065ea9aa3d4b89bc6c8a0c254fe30
free1 <747549945@qq.com>
2015-06-01 17:46:25 +0800

    重写capistrano sunspot

--- config/deploy.rb
+++ config/deploy.rb
@@ -148,33 +148,46 @@ end
 # set :thinking_sphinx_rails_env, -> { fetch(:rails_env) || fetch(:stage) }
 
 # sunspot
-namespace :deploy do
-  before :updated, :setup_solr_data_dir do
-    on roles(:app) do
-      unless test "[ -d #{shared_path}/solr/data ]"
-        execute :mkdir, "-p #{shared_path}/solr/data"
-      end
-    end
-  end
-end
+# namespace :deploy do
+#   before :updated, :setup_solr_data_dir do
+#     on roles(:app) do
+#       unless test "[ -d #{shared_path}/solr/data ]"
+#         execute :mkdir, "-p #{shared_path}/solr/data"
+#       end
+#     end
+#   end
+# end
 
 namespace :solr do
   
+  # %w[start stop].each do |command|
+  #   desc "#{command} solr"
+  #   task command do
+  #     on roles(:app) do
+  #       solr_pid = "#{shared_path}/solr/pids/sunspot-solr.pid"
+  #       if command == "start" or (test "[ -f #{solr_pid} ]" and test "kill -0 $( cat #{solr_pid} )")
+  #         within current_path do
+  #           with rails_env: fetch(:rails_env, 'production') do
+  #             # execute :bundle, 'exec', 'sunspot-solr', command, "--port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/solr/pids"
+  #             execute :bundle, 'exec', 'sunspot-solr', command, "--pid-dir=#{shared_path}/solr/pids"
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+
   %w[start stop].each do |command|
-    desc "#{command} solr"
-    task command do
-      on roles(:app) do
-        solr_pid = "#{shared_path}/solr/pids/sunspot-solr.pid"
-        if command == "start" or (test "[ -f #{solr_pid} ]" and test "kill -0 $( cat #{solr_pid} )")
-          within current_path do
-            with rails_env: fetch(:rails_env, 'production') do
-              # execute :bundle, 'exec', 'sunspot-solr', command, "--port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/solr/pids"
-              execute :bundle, 'exec', 'sunspot-solr', command, "--pid-dir=#{shared_path}/solr/pids"
-            end
-          end
-        end
-      end
-    end
+  	desc "#{command} sunspot"
+  	task command do
+  		on roles(:app) do
+  			within current_path do
+  				with rails_env: fetch(:rails_env, 'production') do
+  					execute :bundle, 'exec', :rake, "sunspot:solr:#{command}"
+  				end
+  			end
+  		end
+  	end
   end
   
   desc "重启 sunspot"
@@ -192,14 +205,7 @@ namespace :solr do
       execute :rm, "-rf #{current_path}/solr/production/data"
     end
     invoke 'solr:start'
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env, 'production') do
-          info "Reindexing Solr database"
-          execute :bundle, 'exec', :rake, 'sunspot:solr:reindex'
-        end
-      end
-    end
+    execute :bundle, 'exec', :rake, "sunspot:solr:reindex"
   end
 
 end
