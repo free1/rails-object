78d7923842ea0dd6205d18b9abcb46dfb151353d
free1 <747549945@qq.com>
2015-06-02 13:58:22 +0800

    sunspot capistrano 没成功，暂时把data放入current中

--- config/deploy.rb
+++ config/deploy.rb
@@ -161,46 +161,60 @@ end
 
 namespace :solr do
   
-  %w[start stop].each do |command|
-    desc "#{command} solr"
-    task command do
-      on roles(:app) do
-        solr_pid = "#{shared_path}/pids/sunspot-solr.pid"
-        if command == "start" or (test "[ -f #{solr_pid} ]" and test "kill -0 $( cat #{solr_pid} )")
-          within current_path do
-            with rails_env: fetch(:rails_env, 'production') do
-              execute :bundle, 'exec', 'sunspot-solr', command, "--port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
-            end
-          end
-        end
-      end
-    end
-  end
+  # %w[start stop].each do |command|
+  #   desc "#{command} solr"
+  #   task command do
+  #     on roles(:app) do
+  #       solr_pid = "#{shared_path}/pids/sunspot-solr.pid"
+  #       if command == "start" or (test "[ -f #{solr_pid} ]" and test "kill -0 $( cat #{solr_pid} )")
+  #         within current_path do
+  #           with rails_env: fetch(:rails_env, 'production') do
+  #             execute :bundle, 'exec', 'sunspot-solr', command, "--port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
   
   desc "重启 sunspot"
   task :restart do
     invoke 'solr:stop'
     invoke 'solr:start'
   end
-  
-  after 'deploy:finished', 'solr:restart'
-  
-  desc "reindex sunspot"
-  task :reindex do
-    invoke 'solr:stop'
-    on roles(:app) do
-      execute :rm, "-rf #{shared_path}/solr/data"
-    end
-    invoke 'solr:start'
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env, 'production') do
-          info "Reindexing Solr database"
-          execute :bundle, 'exec', :rake, 'sunspot:solr:reindex[,,true]'
+
+  %w[start stop].each do |command|
+    desc "#{command} solr"
+    task command do
+      on roles(:app) do
+        within current_path do
+          with rails_env: fetch(:rails_env, 'production') do
+            execute :bundle, 'exec', :rake, "sunspot:solr:#{command}"
+          end
         end
       end
     end
   end
+  
+  # reindex 路径有问题
+  # after 'deploy:finished', 'solr:restart'
+  
+  # desc "reindex sunspot"
+  # task :reindex do
+  #   invoke 'solr:stop'
+  #   on roles(:app) do
+  #     execute :rm, "-rf #{shared_path}/solr/data"
+  #   end
+  #   invoke 'solr:start'
+  #   on roles(:app) do
+  #     within current_path do
+  #       with rails_env: fetch(:rails_env, 'production') do
+  #         info "Reindexing Solr database"
+  #         execute :bundle, 'exec', :rake, 'sunspot:solr:reindex[,,true]'
+  #       end
+  #     end
+  #   end
+  # end
 
 end
 
