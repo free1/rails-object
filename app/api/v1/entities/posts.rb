039931c80b7bcc34e79dd3e1db1ad7e46bad9140
free1 <747549945@qq.com>
2015-06-07 11:57:27 +0800

    api post去掉不必要的user

--- app/api/v1/entities/posts.rb
+++ app/api/v1/entities/posts.rb
@@ -2,7 +2,7 @@ module V1
   module Entities
     class Posts < Grape::Entity
       expose :id, :title, :content, :created_at, :cover_path, :source_site
-      expose :user, using: V1::Entities::User::Users
+      # expose :user, using: V1::Entities::User::Users
     end
   end
 end
