67e1ac0b922ff86608b9e470fb1c6c1c7db7d912
free1 <747549945@qq.com>
2015-06-07 11:50:06 +0800

    api post加上源站

--- app/api/v1/entities/posts.rb
+++ app/api/v1/entities/posts.rb
@@ -1,7 +1,7 @@
 module V1
   module Entities
     class Posts < Grape::Entity
-      expose :id, :title, :content, :created_at, :cover_path
+      expose :id, :title, :content, :created_at, :cover_path, :source_site
       expose :user, using: V1::Entities::User::Users
     end
   end
