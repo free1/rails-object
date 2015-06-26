e9e3158f59efa2aef079508914d1a992942fb558
free1 <747549945@qq.com>
2015-06-26 18:05:31 +0800

    test change domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_weixin_test_session'
+Rails.application.config.session_store :cookie_store, key: '_weixin_test_session', domain: '121.42.161.252'
