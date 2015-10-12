fd9c7b2dd4187d72cacfc47668c7d5607c8fc6c7
free <free@makehave.com>
2015-10-12 21:43:48 +0800

    去掉redis密码

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,8 +5,8 @@ require "redis/objects"
 
 if Rails.env.production?
   redis_configuration = {host: ENV['redis_host'],
-                        port: ENV['redis_port'], 
-                        password: ENV['redis_password']}
+                        port: ENV['redis_port']}
+                        # password: ENV['redis_password']}
   #redis-objects-config
   $redis = Redis::Objects.redis = Redis.new(redis_configuration)
 else
