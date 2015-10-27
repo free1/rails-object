cae09b10aafd7efab61be8d70bc9e11845b25b8d
free <free@makehave.com>
2015-10-27 09:44:58 +0800

    web 更新新版主页

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-  	unless signed_in?
-  		render 'cover_home', layout: false
-  	end
+    unless signed_in?
+        # render 'cover_home', layout: false
+        render 'cover_home_v2', layout: false
+    end
   end
 end
