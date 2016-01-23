6b9f5db8857cb8f5cb5fc9128bde4f3b35c401d5
free <free@makehave.com>
2016-01-23 11:54:04 +0800

    mobile font  asset test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( mobile_application.js )
 Rails.application.config.assets.precompile += %w( mobile_application.css )
+
+Rails.application.config.assets.paths << Rails.root.join('assets', 'mobile', 'fonts')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
