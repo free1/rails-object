233c75090cc14c796c525333f5f8c848c2323f61
free <free@makehave.com>
2016-02-15 10:56:38 +0800

    mobile 首页热门推荐栏目

--- app/assets/javascripts/mobile/pages/Index.js
+++ app/assets/javascripts/mobile/pages/Index.js
@@ -9,9 +9,16 @@ import {
 import {
   Link,
 } from 'react-router';
+import {
+  Grid,
+  Col,
+  AvgGrid,
+  Titlebar,
+  Panel,
+} from 'amazeui-react';
 
 const sliderIntance = (
-  <Slider>
+  <Slider >
     <Slider.Item>
       <img src="http://s.amazeui.org/media/i/demos/bing-1.jpg" />
     </Slider.Item>
@@ -25,6 +32,19 @@ const sliderIntance = (
       <img src="http://s.amazeui.org/media/i/demos/bing-4.jpg" />
     </Slider.Item>
   </Slider>
+);
+
+var avgGridInstance = (
+  <AvgGrid sm={2} md={3} lg={4} className="am-thumbnails">
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-1.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-2.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-3.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-4.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-1.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-2.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-3.jpg" /></li>
+    <li><img className="am-thumbnail" src="http://s.amazeui.org/media/i/demos/bing-4.jpg" /></li>
+  </AvgGrid>
 );
 
 const Index = React.createClass({
@@ -41,7 +61,20 @@ const Index = React.createClass({
           amStyle="primary"
           title="React Demo"
         />
-          {sliderIntance}
+          <Grid className="doc-g centre-body">
+            <Col md={8} mdOffset={2} >
+              <div>
+                {sliderIntance}
+              </div>
+              
+              <div>
+                <Titlebar title="热门推荐" />
+                <Panel>
+                  {avgGridInstance}
+                </Panel>
+              </div>
+            </Col>
+          </Grid>
       </View>
     );
   },
