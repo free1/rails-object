ca4e153f85938988a3c925e6483040ca3f593799
free <free@makehave.com>
2016-02-24 11:21:45 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-  config.assets.js_compressor = Uglifier.new(output: {ascii_only: true, quote_keys: true})
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
