c2fa897919b64c6742f118405714893d436eb66f
free <free@makehave.com>
2016-02-25 17:07:02 +0800

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( mobile_application.js )
-Rails.application.config.assets.precompile += %w( mobile_application.css )
+# Rails.application.config.assets.precompile += %w( mobile_application.js )
+# Rails.application.config.assets.precompile += %w( mobile_application.css )
 
 # Rails.application.config.assets.paths << Rails.root.join('assets', 'mobile', 'fonts')
 # Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
