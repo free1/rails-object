8ba5b2b4383bbeba97d9f0aff8bd8a6868f82108
free <free@makehave.com>
2016-04-11 17:11:12 +0800

    [api] 首页顶部滚动

--- app/api_views/new_api/v2/home/index.json.jbuilder
+++ app/api_views/new_api/v2/home/index.json.jbuilder
@@ -1,3 +1,4 @@
 json.data do
   json.hot_products @hot_products
+  json.roll_nav_infos @roll_nav_infos
 end
--- app/controllers/new_api/v2/home_controller.rb
+++ app/controllers/new_api/v2/home_controller.rb
@@ -2,6 +2,7 @@ class NewApi::V2::HomeController < NewApi::V2::BaseController
 
   def index
     @hot_products = Product.hot.order(id: :desc).last(8)
+    @roll_nav_infos = RollNavInfo.order(:weight).last(6)
   end
 
 end
